4d9f9d8438a0b5572fc10870bfe40b51d983abb2
sjullieb <sjullieb@gmail.com>
2019-05-28 19:43:42 -0700

    add Toast on button click

--- app/src/main/java/com/example/guest/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/guest/myrestaurants/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.guest.myrestaurants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantButton);
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
